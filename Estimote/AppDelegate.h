fa8db36d6488fb3f27990ad34296211f5d66182b
lemettre <lemettre@Mac-mini-de-Bruno-QUINTIN.local>
2014-05-21 08:31:04 +0200

    Initial Commit

---
+++ Estimote/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Estimote
+//
+//  Created by lemettre on 21/05/14.
+//  Copyright (c) 2014 lemettre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Estimote/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Estimote
+//
+//  Created by lemettre on 21/05/14.
+//  Copyright (c) 2014 lemettre. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
